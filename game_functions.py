28b8b9f58c514c1f66acd7bcfa5798548eab8bc6
Sia-M-1 <sia.razumovskaya@mail.ru>
2025-05-16 21:35:27 +0300

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -1,21 +1,24 @@
 import sys
 import pygame
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-                # Переместить корабль вправо.
-                ship.rect.centerx += 1
+            check_keyup_events(event, ship)
+
 def update_screen(ai_settings, screen, ship):
     # При каждом проходе цикла перерисовывается экран.
     screen.fill(ai_settings.bg_color)
