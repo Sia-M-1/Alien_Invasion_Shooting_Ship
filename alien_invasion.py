9c6dd8e2cf7329f4cf18f2ef2a1780c046a1321b
Sia-M-1 <sia.razumovskaya@mail.ru>
2025-05-17 18:30:46 +0300

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 # Запуск основного цикла игры.
 def run_game():
     pygame.init()
@@ -11,9 +12,13 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     # Создание корабля.
     ship = Ship(ai_settings,screen)
-    while True:
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+    # Создание группы для хранения пуль.
+    bullets = Group()
 
-run_game()
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game( )
